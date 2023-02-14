d8d1198c0ebb63ac64e6fcc6a6ba6c9ee9244671
nikjk <nik.kuehn@outlook.com>
2023-02-13 21:34:47 -0500

    Add project files.

---
+++ Unit4-CircleObjectsLab/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit4_CircleObjectsLab
+{
+    public class Circle
+    {
+        private double _radius;
+
+        public double Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+
+        private string FormatNumber(double x)
+        {
+            return x.ToString("0.##");
+        }
+    }
+}
---
+++ Unit4-CircleObjectsLab/Program.cs
@@ -0,0 +1,26 @@
+﻿using Unit4_CircleObjectsLab;
+
+bool doAgain = false;
+do
+{
+    Console.WriteLine("Enter the radius of the circle:");
+    double radius = double.Parse(Console.ReadLine());
+
+    Circle circle = new Circle(radius);
+
+    Console.WriteLine("Circumference: " + circle.CalculateFormattedCircumference());
+    Console.WriteLine("Area: " + circle.CalculateFormattedArea());
+    Console.WriteLine("Would you like to go again?");
+    string userInput = Console.ReadLine();
+    if (userInput.ToLower() == "y")
+    {
+        doAgain = true;
+    }
+    else
+    {
+        doAgain = false;
+    }
+} while (doAgain == true);
+
+
+Console.WriteLine("Goodbye");
